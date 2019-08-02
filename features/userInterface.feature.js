523668f4d735663aa4ab44db9bc90594a4320ecd
Daria Lykova <daria.lykova@gmail.com>
2019-08-02 22:38:46 +0200

    Tests for page to have rock, paper & scissors icons

--- features/userInterface.feature.js
+++ features/userInterface.feature.js
@@ -5,6 +5,7 @@ context('Your Description of the test scenario', () => {
   before(async () => {
     await browser.init()
     await browser.visitPage('http://localhost:8080/')
+  
   });
 
   // Reload before each test 
@@ -18,6 +19,16 @@ context('Your Description of the test scenario', () => {
   });
 
   it('contains 3 icons', async () => {
+    await browser.page.waitFor(1500)
     expect(await browser.page.$$('.choice')).to.have.lengthOf(3)
+  });
+  it('contains rock icon', async () => {
+    expect(await browser.page.$$('.fa-hand-rock')).to.have.lengthOf(1)
+  });
+  it('contains paper icon', async () => {
+    expect(await browser.page.$$('.fa-hand-paper')).to.have.lengthOf(1)
+  });
+  it('contains scissors icon', async () => {
+    expect(await browser.page.$$('.fa-hand-scissors')).to.have.lengthOf(1)
   });
 });
