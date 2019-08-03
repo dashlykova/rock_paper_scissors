a5a6e28875c64aba027c4b43282aa6decd2dffd7
Daria Lykova <daria.lykova@gmail.com>
2019-08-03 13:02:48 +0200

    Passes 'displays computer choice upon making a move' test

--- features/userInterface.feature.js
+++ features/userInterface.feature.js
@@ -18,26 +18,33 @@ context('Your Description of the test scenario', () => {
     browser.close();
   });
 
-  it('contains 3 icons', async () => {
-    await browser.page.waitFor(1500)
-    expect(await browser.page.$$('.choice')).to.have.lengthOf(3)
+  describe('UI', () => {
+    it('contains 3 icons', async () => {
+      await browser.page.waitFor(1500)
+      expect(await browser.page.$$('.choice')).to.have.lengthOf(3)
+    });
+  
+    it('contains a rock icon', async () => {
+      expect(await browser.page.$$('.fa-hand-rock')).to.have.lengthOf(1)
+    });
+  
+    it('contains a paper icon', async () => {
+      expect(await browser.page.$$('.fa-hand-paper')).to.have.lengthOf(1)
+    });
+  
+    it('contains a scissors icon', async () => {
+      expect(await browser.page.$$('.fa-hand-scissors')).to.have.lengthOf(1)
+    });
   });
-  it('contains a rock icon', async () => {
-    expect(await browser.page.$$('.fa-hand-rock')).to.have.lengthOf(1)
-  });
-  it('contains a paper icon', async () => {
-    expect(await browser.page.$$('.fa-hand-paper')).to.have.lengthOf(1)
-  });
-  it('contains a scissors icon', async () => {
-    expect(await browser.page.$$('.fa-hand-scissors')).to.have.lengthOf(1)
-  });
+
+  
   it('displays computer choice upon making a move', async () => {
     await browser.clickOnButton("i[id='scissors']")
     // await browser.page.waitFor(1500)
-    let content = await browser.getContent("div[id='result']")
-    let possibleFeedback = /(?: chose rock|chose scissors|chose paper)/
+    let content = await browser.getContent("div[id='result'] p")
+    let possibleFeedback = /Computer (?: chose rock|chose scissors|chose paper)/
     console.log(content)
-    expect(possibleFeedback.test(content.innerText)).to.equal(true) 
+    expect(possibleFeedback.test(content)).to.equal(true) 
   });
   xit('shows result upon clicking on an icon', async () => {
     await browser.clickOnButton("i[id='scissors']")
